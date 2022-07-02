23e90eddfad2d445f1b73e6e8367165ece82b991
Abdulrahman Ojuromi <64367079+AbdRahman001@users.noreply.github.com>
2022-07-02 21:56:03 +0100

    Add files via upload

---
+++ I46012229Q8J/Links/serializer.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from .models import Link
+from rest_framework import serializers
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = "__all__"
+        
